487e61a9d9aafe2b3287f2116472d3405637f144
Chippasaur <alvinchungg8181@gmail.com>
2021-03-22 23:19:38 +0800

    feat: add notification render test

---
+++ components/notifications/index.test.tsx
@@ -0,0 +1,20 @@
+import { render } from 'enzyme'
+import Notifications from './index'
+import { NotificationType } from './NotificationType'
+
+describe('<Notifications />', () => {
+  const notifications = [
+    {
+      supplierName: 'company1',
+      type: NotificationType.INVITATION,
+      createdTime: new Date('2021-2-23 16:00:00'),
+    },
+    { supplierName: 'company2', type: NotificationType.SEND_MESSAGE, createdTime: new Date('2021-2-23 16:00:00') },
+    { supplierName: 'company3', type: NotificationType.SUBMIT_SURVEY, createdTime: new Date('2021-2-23 16:00:00') },
+  ]
+
+  it('render 3 notifications', () => {
+    const component = render(<Notifications notifications={notifications} />)
+    expect(component.find('.supplierName')).toHaveLength(3)
+  })
+})
