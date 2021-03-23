373d5b5e481f0f0c3244569540591db620e26520
Chippasaur <alvinchungg8181@gmail.com>
2021-03-23 10:23:48 +0800

    feat: add news model

---
+++ models/News.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const newSchema = new mongoose.Schema({
+  companyId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    requred: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  lastUpdatedAt: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+})
+
+const model = mongoose.model('News', newSchema)
+
+export default model
