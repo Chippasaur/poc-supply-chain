314f03ad2e3f1b113d3c39ab3fb7da0c8f54e18e
Alvin Chung <alvin.chung8181@gmail.com>
2021-03-27 20:24:06 +0800

    feat: add images and navBar

--- components/navigationBar/index.tsx
+++ components/navigationBar/index.tsx
@@ -3,9 +3,16 @@ import { memo } from 'react'
 import Item, { NavigationItem } from './item'
 import { useRouter } from 'next/router'
 import map from 'lodash/map'
+import Image from 'next/image'
 
 const Navigation: Array<NavigationItem> = [
   { title: 'Dashboard', url: '/' },
+  { title: 'Solutions', url: '/test' },
+  { title: 'Profile', url: '/test' },
+  { title: 'Quotations', url: '/test' },
+  { title: 'Connections', url: '/test' },
+  { title: 'Products', url: '/test' },
+  { title: 'Surveys', url: '/test' },
   { title: 'Counterparties', url: '/counterparties' },
 ]
 
@@ -14,12 +21,14 @@ const NavigationBar = () => {
   console.log(router)
   const currentPath = router.pathname
   return (
-    <div className={styles.NavigationBar}>
+    <div className={styles.navigationBar}>
+      <Image src="/images/logo.png" width={60} height={60} className={styles.img} />
       <div className={styles.menus}>
         {map(Navigation, ({ title, url }) => (
-          <Item key={url} title={title} url={url} isActive={currentPath === url} />
+          <Item key={title} title={title} url={url} isActive={currentPath === url} />
         ))}
       </div>
+      <div className={styles.message}> right area</div>
     </div>
   )
 }
