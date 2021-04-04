a4672402a7d651dc7847238e606470a8091c7c05
Alvin Chung <alvin.chung8181@gmail.com>
2021-04-04 16:39:47 +0800

    fix: adjust folder location & use swr to request data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,13 @@
 import Head from 'next/head'
 import Notifications from '../components/notifications'
-import { NotificationType } from '../components/notifications/NotificationType'
 import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
 
-const notifications = [
-  {
-    supplierName: 'company1',
-    type: NotificationType.INVITATION,
-    createdTime: new Date('2021-2-23 16:00:00'),
-  },
-  {
-    supplierName: 'company2',
-    type: NotificationType.INVITATION,
-    createdTime: new Date('2021-2-23 13:00:00'),
-  },
-  {
-    supplierName: 'company3',
-    type: NotificationType.INVITATION,
-    createdTime: new Date('2021-2-23 11:00:00'),
-  },
-]
+const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 export default function Home() {
+  const { data: notifications = [] } = useSWR('/api/notifications', fetcher)
+
   return (
     <div className={styles.container}>
       <Head>
@@ -30,46 +16,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Greeting, Alvin</h1>
         <Notifications notifications={notifications} />
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
--- tests/pages/index.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../../pages'
-
-describe('Home', () => {
-  it('render without crashing', () => {
-    render(<Home />)
-    expect(screen.getByRole('heading', { name: 'Greeting, Alvin' })).toBeInTheDocument
-  })
-})
