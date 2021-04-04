76906099e878a9d4cb9d32571efa06bad505c068
Alvin Chung <alvin.chung8181@gmail.com>
2021-04-04 21:05:19 +0800

    add material ui & basic styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import Head from 'next/head'
 import Notifications from '../components/notifications'
 import styles from './index.module.scss'
+import React from 'react'
+import Button from '@material-ui/core/Button'
+import Avatar from '@material-ui/core/Avatar'
 import useSWR from 'swr'
 
 const fetcher = (url: string) => fetch(url).then(res => res.json())
 
-const user = { name: 'Matt' }
+const user = { name: 'Matt', company: 'Amazon, Inc' }
+const companyInfo = { avatar: 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Amazon_Alexa_App_Logo.png' }
 
 export default function Home() {
   const { data: notifications = [] } = useSWR('/api/notifications', fetcher)
 
-  const { name } = user
+  const { name, company } = user
+  const { avatar } = companyInfo
 
   return (
     <div className={styles.container}>
@@ -20,8 +25,21 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <div className={styles.header}>
+          <div>
+            <div className={styles.companyInfo}>
+              <Avatar alt="Remy Sharp" src={avatar} />
+              <p>{company}</p>
+            </div>
+          </div>
+        </div>
         <div>
-          <h1 className={styles.title}> Welcome Back, {name}</h1>
+          <Button variant="outlined" color="primary">
+            Add colleague
+          </Button>
+          <Button variant="outlined" color="primary">
+            Invite counterparty
+          </Button>
         </div>
         <Notifications notifications={notifications} />
       </main>
