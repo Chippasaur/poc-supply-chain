5441e92da7987c629a359284cd3e1c004fe9b6ec
Alvin Chung <alvin.chung8181@gmail.com>
2021-04-04 16:08:15 +0800

    feat: add notification

---
+++ migrations/20210404080504-insert-data-to-notification.js
@@ -0,0 +1,44 @@
+module.exports = {
+  async up(db, client) {
+    try {
+      for (let i = 0; i < 10; i++) {
+        await db.insert('notifications', {
+          supplierName: 'COOPERATIVA TEXTIL SEUCA LTDA.',
+          type: i % 5,
+          createTime: new Date('2021-1-23 16:00:00 UTC'),
+        })
+      }
+      for (let i = 0; i < 10; i++) {
+        await db.insert('notifications', {
+          supplierName: 'FE NEW CENTURY INDUSTRY (SINGAPORE) PTE LTD',
+          type: i % 5,
+          createTime: new Date('2021-2-23 16:00:00 UTC'),
+        })
+      }
+      for (let i = 0; i < 10; i++) {
+        await db.insert('notifications', {
+          supplierName: 'GOLD LONG JOHN CHINA',
+          type: i % 5,
+          createTime: new Date('2021-2-2 16:30:00 UTC'),
+        })
+      }
+      for (let i = 0; i < 10; i++) {
+        await db.insert('notifications', {
+          supplierName: 'JIANSHAN BENHUI PRINTING CO., LTD',
+          type: i % 5,
+          createTime: new Date('2021-2-10 8:00:00 UTC'),
+        })
+      }
+    } catch (error) {
+      console.error('migration error: ', error)
+    }
+  },
+
+  async down(db, client) {
+    try {
+      await db.dropCollection('notifications')
+    } catch (error) {
+      console.error('migration error: ', error)
+    }
+  },
+}
