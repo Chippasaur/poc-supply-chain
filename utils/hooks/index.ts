9ae24d0764dd818a1923a1c5658032a34b61012b
Alvin Chung <alvin.chung8181@gmail.com>
2021-04-07 19:13:28 +0800

    style: add custom fonts

--- utils/hooks/index.ts
+++ utils/hooks/index.ts
@@ -4,5 +4,6 @@ import { fetcher } from '../api'
 export const useCompanyData = () => {
   const { data: { name, companyName } = {} } = useSWR('/api/users', fetcher)
   const { data: { logoUrl, ...datas } = {} } = useSWR(`/api/companies?companyName=${companyName}`, fetcher)
+
   return { name, companyName, logoUrl, ...datas }
 }
